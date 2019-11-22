6bcf8c4fbfe3e5ed1d81429b33ae2ec0691157eb
Stu Tily <tily@subatomic.com>
2019-11-22 10:18:20 +0100

    remove unused definition

--- eks/main.tf
+++ eks/main.tf
@@ -6,12 +6,3 @@ provider "aws" {
   region  = var.aws_region
 }
 
-
-# data "terraform_remote_state" "vpc" {
-#   backend = "s3"
-#   config = {
-#     bucket = "subatomic-dev-gsp-state-2"
-#     key = "vpc/terraform.tfstate"
-#     region = "eu-west-2"
-#   }
-# }
